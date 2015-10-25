54d4e0e68df7f66305d956ee8dd1b181ca13cab9
Prashant Jalan <pj.prashantjalan@gmail.com>
2015-10-25 23:02:52 +0530

    Fetching images before page loading

--- js/counsel.js
+++ js/counsel.js
@@ -13,9 +13,21 @@ $.vegas('slideshow', {
 /* =================================
    LOADER                     
 =================================== */
+function preload(arrayOfImages) {
+    $(arrayOfImages).each(function(){
+        $('<img/>')[0].src = this;
+        // Alternatively you could use:
+        // (new Image()).src = this;
+    });
+}
 
 // makes sure the whole site is loaded
 jQuery(window).load(function() {
+  preload([
+    'images/backgrounds/bg1.jpg',
+    'images/backgrounds/bg2.jpg',
+    'images/backgrounds/bg3.jpg'
+  ]);
   // will first fade out the loading animation
 	jQuery(".status").fadeOut();
   // will fade out the whole DIV that covers the website.
